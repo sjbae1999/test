00b65e632146b6fe990e7b275023388fdc36bc8b
Seongjong <sjbae1999@dgist.ac.kr>
2024-02-20 15:06:24 +0900

    first file

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string>
+
+int main()
+{
+    std::string name_1 = "jeongyoon";
+    std::string name_2 = "seongjong";
+    printf("%s, %s\n", name_1.c_str(), name_2.c_str());
+}
